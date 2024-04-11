a53f2940e2b125c4b2d901c8b78802499c526b41
nadav.v <nadav.v@taboola.com>
2024-04-11 21:18:22 +0300

    valid bst, web tracking

---
+++ src/main/java/priorityqueues/WebTracking.java
@@ -0,0 +1,10 @@
+package priorityqueues;
+
+import java.util.Map;
+
+public class WebTracking {
+    Map<String, Integer> websiteToTotalUsers;
+    Map<String, Map<String, Integer>> userToWebsiteToCounter;
+
+
+}
