c3e5d8f50aa41306b0c6c81e708038998c817e99
nadav.v <nadav.v@taboola.com>
2024-04-11 10:33:10 +0300

    taxicab number

---
+++ src/main/java/priorityqueues/TaxicabNumbers.java
@@ -0,0 +1,32 @@
+package priorityqueues;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaxicabNumbers {
+
+    public void taxicab3(int n) {
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        for (int i = 1; i < 100; i++) {
+            for (int j = i; j < 100; j++) {
+                int sum = i * i * i + j * j * j;
+                if (set.contains(sum)) {
+                    System.out.println(sum);
+                    count++;
+                } else {
+                    set.add(sum);
+                }
+                if (count == n) {
+                    return;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        TaxicabNumbers taxicabNumbers = new TaxicabNumbers();
+        taxicabNumbers.taxicab3(2);
+    }
+}
