0c82492f0e22595591457771a8b0bd05bea1b512
nadav.v <nadav.v@taboola.com>
2024-05-01 21:44:52 +0300

    all traversals

--- src/main/java/part1/priorityqueues/BSTTraversal.java
+++ src/main/java/part1/priorityqueues/BSTTraversal.java
@@ -6,6 +6,10 @@ import java.util.Stack;
 
 public class BSTTraversal {
 
+    public enum Traversal {
+        INORDER, PREORDER, POSTORDER
+    }
+
     public List<Integer> inorderTraversalIterative(TreeNode root) {
         List<Integer> inorder = new ArrayList<>();
         Stack<TreeNode> s = new Stack<>();
@@ -22,10 +26,19 @@ public class BSTTraversal {
         return inorder;
     }
 
-    public List<Integer> inorderTraversalRecursion(TreeNode root) {
-        List<Integer> inorder = new ArrayList<>();
-        inorder(root, inorder);
-        return inorder;
+    public List<Integer> traversalRecursion(TreeNode root, Traversal traversal) {
+        List<Integer> order = new ArrayList<>();
+        switch (traversal) {
+            case INORDER:
+                inorder(root, order);
+                break;
+            case POSTORDER:
+                postOrder(root, order);
+                break;
+            case PREORDER:
+                preOrder(root, order);
+        }
+        return order;
     }
 
     private void inorder(TreeNode root, List<Integer> inorder) {
@@ -37,7 +50,25 @@ public class BSTTraversal {
         inorder(root.right, inorder);
     }
 
-    public class TreeNode {
+    private void preOrder(TreeNode root, List<Integer> inorder) {
+        if (root == null) {
+            return;
+        }
+        inorder.add(root.val);
+        inorder(root.left, inorder);
+        inorder(root.right, inorder);
+    }
+
+    private void postOrder(TreeNode root, List<Integer> inorder) {
+        if (root == null) {
+            return;
+        }
+        inorder.add(root.val);
+        inorder(root.left, inorder);
+        inorder(root.right, inorder);
+    }
+
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -53,6 +84,18 @@ public class BSTTraversal {
             this.val = val;
             this.left = left;
             this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(2, new TreeNode(4), new TreeNode(5));
+        TreeNode right = new TreeNode(3, null, new TreeNode(6));
+        TreeNode root = new TreeNode(1, left, right);
+
+        BSTTraversal bstTraversal = new BSTTraversal();
+        for (Traversal traversal : Traversal.values()) {
+            System.out.println(traversal);
+            System.out.println(bstTraversal.traversalRecursion(root, traversal));
         }
     }
 }
